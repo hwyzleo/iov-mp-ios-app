6e2d20c216cf0fe22cd157863b338bf3831b829d
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-06-03 18:04:21 +0800

    feat: create MarketingRoute module route enum

---
+++ iov/iov/Module/Marketing/MarketingRoute.swift
@@ -0,0 +1,19 @@
+//
+//  MarketingRoute.swift
+//  iov
+//
+//  Created by iov on 2026/6/3.
+//
+
+import SwiftUI
+
+/// 购车模块路由枚举
+enum MarketingRoute: Hashable {
+    case index
+    case modelVariantSelection(saleModelCode: String)
+    case modelConfig(saleModelCode: String)
+    case orderDetail(orderNo: String)
+    case licenseArea
+    case dealership
+    case deliveryCenter
+}
