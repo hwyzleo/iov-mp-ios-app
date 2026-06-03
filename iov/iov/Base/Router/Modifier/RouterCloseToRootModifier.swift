eed9931b5d143ae69539961bf704aed29cb98358
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-06-03 16:17:16 +0800

    fix: 修复返回到根页面的逻辑，多次调用dismiss确保返回到购车首页

--- iov/iov/Base/Router/Modifier/RouterCloseToRootModifier.swift
+++ iov/iov/Base/Router/Modifier/RouterCloseToRootModifier.swift
@@ -17,13 +17,22 @@ struct RouterCloseToRootModifier: ViewModifier {
     // MARK: Private
     
     @Environment(\.dismiss) private var dismiss
+    @State private var shouldDismiss = false
     
     // MARK: Life cycle
     
     func body(content: Content) -> some View {
         content
             .onReceive(publisher) { _ in
+                // 触发多次dismiss以返回到根页面
                 dismiss()
+                // 延迟后再次dismiss，以确保返回到根页面
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    dismiss()
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    dismiss()
+                }
             }
     }
 }
